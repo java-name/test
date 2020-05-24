635b3650ee536dc815810c43eef5fca413d12260
hyhtCloud <1531281540@qq.com>
2020-05-24 23:35:53 +0800

    hello test

---
+++ com/java/Demo.java
@@ -0,0 +1,12 @@
+package java;
+
+/**
+ * @Auther: lqj
+ * @Date: 2020/5/24 23:34
+ * @Description:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello test");
+    }
+}
